f7858777e74e173d296563a4ff6cef128eca87a4
Ilya Tampio <quakumei@gmail.com>
2023-09-12 03:27:05 +0300

    add: copilot

---
+++ after/plugin/copilot.lua
@@ -0,0 +1,2 @@
+-- Using system-wide proxy helps
+vim.g.copilot_proxy = "http://127.0.0.1:1095"
--- lua/quakumei/packer.lua
+++ lua/quakumei/packer.lua
@@ -36,4 +36,5 @@ return require('packer').startup(function(use)
     }
   }
 
+  use ('github/copilot.vim')
 end)
